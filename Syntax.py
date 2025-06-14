a097c89051beaae01f314299472c3e76bac7fc2f
Md Minnatul Hasan <48837543+Minnatul@users.noreply.github.com>
2025-06-14 13:42:58 +0600

    Update Syntax.py

--- Syntax.py
+++ Syntax.py
@@ -2,10 +2,21 @@ import sys
 print("The running python version:",sys.version)
 
 
+# This is a comment line for practicing
+
+
+#Python Indentation
+
+if 10 > 5:
+  print("Ten is greater than Five!")
+
+#Python Variables
+
 x=10
 y='Minnatul'
 
 print('My name is',y,'and I have',x,'friends')
+
 
 
 # This is a comment line for practicing
